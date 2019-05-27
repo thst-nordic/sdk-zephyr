cd216b12cfb906e4f91fe20cbb25159355c0373e
Stilwell, Thomas <Thomas.Stilwell@nordicsemi.no>
2019-05-27 21:04:13 +0200

    rename ci/windows/scripts

--- scripts/ci/windows/run.bat
+++ scripts/ci/windows/run.bat
@@ -26,8 +26,8 @@ call .\zephyr-env.cmd
 cd C:\jenkins\zephyr\scripts
 
 python sanitycheck --build-only --platform nrf52_pca10040 --tag bluetooth --ninja --disable-size-report
-
 echo ERROR_LEVEL = %ERROR_LEVEL%
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
 echo EXITING WITH -20 TO FAIL
 EXIT /B -20
