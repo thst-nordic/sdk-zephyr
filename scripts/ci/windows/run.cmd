4c5b5855ac3525bfb0b4ca738f2be767edfb9021
Stilwell, Thomas <Thomas.Stilwell@nordicsemi.no>
2019-05-27 21:56:08 +0200

    change bat to cmd

--- scripts/ci/windows/run.cmd
+++ scripts/ci/windows/run.cmd
@@ -12,7 +12,5 @@ set GNUARMEMB_TOOLCHAIN_PATH=C:\gnuarmemb
 cd C:\jenkins\zephyr
 call .\zephyr-env.cmd
 
-python scripts\sanitycheck --build-only --platform nrf52_pca10040 --tag bluetooth --ninja --disable-size-report
-
-echo ERROR_LEVEL = %ERROR_LEVEL%
+python scripts\sanitycheck --build-only -p nrf9160_pca10090 -p nrf52_pca10040 -p nrf52840_pca10056 --tag bluetooth --tag introduction --ninja --disable-size-report
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
